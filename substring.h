8a005e935571638bc268e9f49090d0ae92f61054
Erick Barcelos <48017585+brcls@users.noreply.github.com>
2020-09-29 22:43:56 -0300

    Delete substring.h

--- substring.h
+++
@@ -1,5 +0,0 @@
-char* Substitui(char* frase, char* acha, char* troca);
-char* Aloca(char* palavra, int n);
-void Escreve();
-void Imprimir(char *frase);
-void Destroi(char **acha, char **troca, char **frase, char **stringFinal);
