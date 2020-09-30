7a24cac6952fb17240ba0c39c5618274850bba1f
Erick Barcelos <48017585+brcls@users.noreply.github.com>
2020-09-29 22:34:27 -0300

    Trabalho 1
    Substituição de string dentro de um texto.

---
+++ substring.h
@@ -0,0 +1,5 @@
+char* Substitui(char* frase, char* acha, char* troca);
+char* Aloca(char* palavra, int n);
+void Escreve();
+void Imprimir(char *frase);
+void Destroi(char **acha, char **troca, char **frase, char **stringFinal);
